c7ee88ffcc483a0efc8f3b3c9780dfa55559bede
Amardeep Ganguly <amardeepganguly@Amardeeps-MacBook-Air.local>
2024-09-16 00:56:27 +0530

    fetch-user-profile-img

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,13 @@
+import { fetchUserProfileImage } from "@/utils/actions/profileActions";
 import { LuUser2 } from "react-icons/lu";
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchUserProfileImage();
+  if (profileImage) {
+    return (
+      <img src={profileImage} className="w-6 h-6 bg-primary rounded-full" />
+    );
+  }
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
